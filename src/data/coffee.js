4a2bd34703cc112ca33d3e45bc05aaed91287812
Keith Richards <dkrichards86@gmail.com>
2019-01-26 15:27:33 -0500

    Jan updates (#2)
    * Updates CI to dpeloy on publish branch.
    
    * Converts activity descriptions to components

--- src/data/coffee.js
+++ src/data/coffee.js
@@ -1,7 +1,15 @@
+import React from 'react';
+
 const dataset = [
   {
     title: "Joe Van Gogh",
-    description: "This is our Van Gogh to coffee shop. Joe Van Gogh is a local roastery serving up great coffee, ethically sourced and expertly crafted. Grab a cup of Ethiopian pour over and be blown away by the fruity notes.",
+    description: () => (
+      <div>
+        This is our Van Gogh to coffee shop. Joe Van Gogh is a local roastery
+        serving up great coffee, ethically sourced and expertly crafted. Grab a
+        cup of Ethiopian pour over and be blown away by the fruity notes.
+      </div>
+    ),
     image: "joevangogh_400x400.jpeg",
     links: [
       {
@@ -16,7 +24,12 @@ const dataset = [
   },
   {
     title: "Bean Traders",
-    description: "Bean Trader's is a South Durham (our neighborhood) favorite. Keith's favorite is Chocolate Habanero Cold Brew",
+    description: () => (
+      <div>
+        Bean Trader's is a South Durham (our neighborhood) favorite. Keith's
+        favorite is Chocolate Habanero Cold Brew
+      </div>
+    ),
     image: "beantraders_400x400.jpg",
     links: [
       {
@@ -31,7 +44,14 @@ const dataset = [
   },
   {
     title: "Cocoa Cinnamon",
-    description: "Cocoa Cinnamon has 3 locations, you will not be disappointed regardless of location you visit. The orginal Cocoa Cinnamon is 1 block from the Rickhouse. The Lakewood location is home to Cocoa Cinnamon's coffee roastery (4th Dimension Coffee) and super yummy churros. ",
+    description: () => (
+      <div>
+        Cocoa Cinnamon has 3 locations, you will not be disappointed regardless
+        of location you visit. The orginal Cocoa Cinnamon is 1 block from the
+        Rickhouse. The Lakewood location is home to Cocoa Cinnamon's coffee
+        roastery (4th Dimension Coffee) and super yummy churros.
+      </div>
+    ),
     image: "cocoacinnamon_400x400.jpg",
     links: [
       {
@@ -46,7 +66,14 @@ const dataset = [
   },
   {
     title: "The Durham Hotel Coffee Bar",
-    description: "The chic Durham Hotel has a bank vault turned coffee bar in its lobby. The home of the coffee soda, definitely worth the trip! If your feeling especially fancy, add a shot of bourbon. After getting your coffee soda, take a trip to the rooftop lounge for a great view of downtown Durham.",
+    description: () => (
+      <div>
+        The chic Durham Hotel has a bank vault turned coffee bar in its lobby.
+        The home of the coffee soda, definitely worth the trip! If your feeling
+        especially fancy, add a shot of bourbon. After getting your coffee soda,
+        take a trip to the rooftop lounge for a great view of downtown Durham.
+      </div>
+    ),
     image: "durhamhotel_400x400.jpeg",
     links: [
       {
--- src/data/restaurants.js
+++ src/data/restaurants.js
@@ -1,7 +1,16 @@
+import React from 'react';
+
 const dataset = [
   {
     title: "Gonza Tacos y Tequila",
-    description: "Because tacos, what else do you need to know? Gonza makes an ideal date night: guacamole, tacos, and a pitcher of margaritas. Usually followed by a quick disagreement about who gets to finish the margarita and who has to drive home.",
+    description: () => (
+      <div>
+        Because tacos, what else do you need to know? Gonza makes an ideal date
+        night: guacamole, tacos, and a pitcher of margaritas. Usually followed
+        by a quick disagreement about who gets to finish the margarita and who
+        has to drive home.
+      </div>
+    ),
     image: "gonza_400x400.jpg",
     links: [
       {
@@ -16,7 +25,13 @@ const dataset = [
   },
   {
     title: "Bull City Burger and Brewery",
-    description: "If you get a Bull City Burger and Brewery tattoo, you get free burgers for life. We really like their passion for quaility prducts. Try the Duck Frites.",
+    description: () => (
+      <div>
+        If you get a Bull City Burger and Brewery tattoo, you get free burgers
+        for life. We really like their passion for quaility prducts. Try the
+        Duck Frites.
+      </div>
+    ),
     image: "bcbb_400x400.jpg",
     links: [
       {
@@ -31,7 +46,14 @@ const dataset = [
   },
   {
     title: "Pompieri Pizza",
-    description: "Pompieri is a sister restaurant to Bull City Burger and Brewery, serving a Durhamers take on Neopolitan pizza. They also host Family Dinner on Sunday evenings, giving the chefs a chance to experiment and serve up some crazy pies.",
+    description: () => (
+      <div>
+        Pompieri is a sister restaurant to Bull City Burger and Brewery, serving
+        a Durhamers take on Neopolitan pizza. They also host Family Dinner on
+        Sunday evenings, giving the chefs a chance to experiment and serve up
+        some crazy pies.
+      </div>
+    ),
     image: "pompieri_400x400.jpg",
     links: [
       {
@@ -46,7 +68,12 @@ const dataset = [
   },
   {
     title: "Treforni",
-    description: "If you like Neopolitan style pizza straight from a brick oven, this South Durham gem is the place to get it. The olive oil is good enough to drink.",
+    description: () => (
+      <div>
+        If you like Neopolitan style pizza straight from a brick oven, this South
+        Durham gem is the place to get it. The olive oil is good enough to drink.
+      </div>
+    ),
     image: "treforni_400x400.jpg",
     links: [
       {
@@ -61,7 +88,13 @@ const dataset = [
   },
   {
     title: "Guglhupf",
-    description: "Guglhupf's is one of our favorite places to get breakfast. It is a traditional German bakery with incredible pastries. Heidi strongly recommends the chocolate croissant.",
+    description: () => (
+      <div>
+        Guglhupf's is one of our favorite places to get breakfast. It is a
+        traditional German bakery with incredible pastries. Heidi strongly
+        recommends the chocolate croissant.
+      </div>
+    ),
     image: "guglhupf_400x400.jpg",
     links: [
       {
@@ -76,7 +109,12 @@ const dataset = [
   },
   {
     title: "The Parlour",
-    description: "We love The Parlour's artisanal ice creams! Just wait for the post-dinner ice cream social!",
+    description: () => (
+      <div>
+        We love The Parlour's artisanal ice creams! Just wait for the post-dinner
+        ice cream social!
+      </div>
+    ),
     image: "parlour_400x400.jpeg",
     links: [
       {
